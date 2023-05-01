3267f5d4b960d43a28096f2d846b8e622923bcdd
cdmi123 <languagepdf@gmail.com>
2023-05-01 09:05:49 +0530

    ravi

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var nodemailer = require('nodemailer');
+var router = express.Router();
+
+/* GET home page. */
+router.post('/', function(req, res, next) {
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'languagepdf@gmail.com',
+    pass: 'ndukkdbbludpcfna'
+  }
+});
+
+var mailOptions = {
+  from: 'languagepdf@gmail.com',
+  to: req.body.email,
+  subject: 'Sending Email using Node.js',
+  text: 'node js demo'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
+
+});
+
+module.exports = router;
