8f6886c2ed36d9bd240a04f495c4717bc764ceb8
cdmi123 <languagepdf@gmail.com>
2023-05-01 09:12:02 +0530

    msg

--- routes/index.js
+++ routes/index.js
@@ -24,7 +24,7 @@ transporter.sendMail(mailOptions, function(error, info){
   if (error) {
     console.log(error);
   } else {
-    console.log('Email sent: ' + info.response);
+    res.send('Email sent: ' + info.response);
   }
 });
 
